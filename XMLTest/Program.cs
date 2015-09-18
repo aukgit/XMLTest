4a92037319f9f11891bb4babc73b7a2200a65433
Alim Ul Karim <devorg.bd@gmail.com>
2015-09-18 11:08:15 +0600

    Dynamically xml retrieved
    Dynamically xml retrieved

--- XMLTest/Program.cs
+++ XMLTest/Program.cs
@@ -1,24 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Xml;
 
 namespace XMLTest {
     class Program {
-        static void Main(string[] args) {
-            var xmlString = File.ReadAllText(@"test.xml");
-            XmlDocument xmlDoc = new XmlDocument();
+        static List<T> GetXml<T>(string xmlString, string typeName) where T : new() {
+            var xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(xmlString);
 
-            string xpath = "Students/Student";
-            var nodes = xmlDoc.SelectNodes(xpath);
+            string xpath = typeName + "s/" + typeName;
 
-            foreach (XmlNode childrenNodeContainer in nodes) {
-                var childs = childrenNodeContainer.ChildNodes;
-                Console.WriteLine();
-                foreach (XmlElement child in childs) {
-                    Console.WriteLine(child.Name + " : " + child.InnerText);
+            var nodes = xmlDoc.SelectNodes(xpath);
+            if (nodes != null) {
+                var list = new List<T>(nodes.Count);
+                foreach (XmlNode childrenNodeContainer in nodes) {
+                    var listItem = new T(); // creating a new instance of target object.
+                    var childs = childrenNodeContainer.ChildNodes;
+                    foreach (XmlElement child in childs) {
+                        var propertyName = child.Name;
+
+                        var propertyInfo = typeof(T).GetProperty(propertyName);
+                        if (propertyInfo != null) {
+                            object value = child.InnerText;
+                            propertyInfo.SetValue(listItem, value, null);
+                        }
+                    }
+                    list.Add(listItem);
                 }
-            } 
+                return list;
+            }
+            return null;
+        }
+        static void Main(string[] args) {
+            var xmlString = File.ReadAllText(@"test.xml");
+            var list = GetXml<Student>(xmlString, "Student");
+            Student.PrintList(list);
             Console.ReadKey();
         }
     }
---
+++ XMLTest/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLTest {
+    class Student {
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public static void PrintList(List<Student> students) {
+            foreach (var student in students) {
+                Console.WriteLine("Name : " + student.Name + " , Loc : " + student.Location);
+            }
+        }
+    }
+}
