a5537b11dfc3d2bc890efb1017b3d88635cd8745
Alim Ul Karim <devorg.bd@gmail.com>
2015-09-18 09:36:52 +0600

    Getting xml data.

--- XMLTest/Program.cs
+++ XMLTest/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
 
 namespace XMLTest {
     class Program {
         static void Main(string[] args) {
+            var xmlString = File.ReadAllText(@"test.xml");
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlString);
 
+            string xpath = "Students/Student";
+            var nodes = xmlDoc.SelectNodes(xpath);
+
+            foreach (XmlNode childrenNodeContainer in nodes) {
+                var childs = childrenNodeContainer.ChildNodes;
+                Console.WriteLine();
+                foreach (XmlElement child in childs) {
+                    Console.WriteLine(child.Name + " : " + child.InnerText);
+                }
+            } 
+            Console.ReadKey();
         }
     }
 }
