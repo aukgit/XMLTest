99945d7c033483c9c6ef76938a870c33bb0ba647
Alim Ul Karim <devorg.bd@gmail.com>
2015-09-18 20:53:09 +0600

    student updated

--- XMLTest/Student.cs
+++ XMLTest/Student.cs
@@ -4,11 +4,8 @@ using System.Xml.Serialization;
 
 namespace XMLTest {
     [Serializable]
-    [XmlType("Student")]
     public class Student {
-        [XmlElement("Name")]
         public string Name { get; set; }
-        [XmlElement("Location")]
         public string Location { get; set; }
 
         public static void PrintList(List<Student> students) {
