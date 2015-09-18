8d0e77fed6737bd2c8045675cb0729f8d5c33fb3
Alim Ul Karim <devorg.bd@gmail.com>
2015-09-18 16:04:19 +0600

    added XML export.
    added XML export.

--- XMLTest/Program.cs
+++ XMLTest/Program.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
+using System.Xml.Serialization;
 
 namespace XMLTest {
     class Program {
-        static List<T> GetXml<T>(string xmlString, string typeName) where T : new() {
+        static List<T> GetXmlToList<T>(string xmlString, string typeName) where T : new() {
             var xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(xmlString);
 
-            string xpath = typeName + "s/" + typeName;
+            var xpath = typeName + "s/" + typeName;
 
             var nodes = xmlDoc.SelectNodes(xpath);
+            if (nodes == null || nodes.Count == 0) {
+                xpath = "ArrayOf" + typeName + "/" + typeName;
+                nodes = xmlDoc.SelectNodes(xpath);
+            }
             if (nodes != null) {
                 var list = new List<T>(nodes.Count);
                 foreach (XmlNode childrenNodeContainer in nodes) {
@@ -32,10 +37,40 @@ namespace XMLTest {
             }
             return null;
         }
+        //static string GetListToXml<T>(T list, string typeName) {
+        //    const BindingFlags typeOfPropertise = BindingFlags.Public | BindingFlags.Instance;
+        //    var propertise =
+        //                typeof(T)
+        //                .GetProperties(typeOfPropertise)
+        //                .ToList();
+        //    var sb = new StringBuilder(propertise.Count + 2);
+        //    var xml = new XElement(typeName + "s", );
+        //}
+
+        public static string GetListToXml2<T>(T obj) {
+            var stringWriter = new StringWriter();
+            var tempXml = new XmlSerializer(typeof(T));
+            tempXml.Serialize(stringWriter, obj);
+            var tempStr = stringWriter.ToString();
+            return tempStr;
+        }
+
+        public class X {
+            public List<Student> Students { get; set; }
+        }
         static void Main(string[] args) {
-            var xmlString = File.ReadAllText(@"test.xml");
-            var list = GetXml<Student>(xmlString, "Student");
+            var fileName = @"test.xml";
+
+            var xmlString = File.ReadAllText(fileName);
+            var list = GetXmlToList<Student>(xmlString, "Student");
             Student.PrintList(list);
+
+            //var x = new X() {
+            //    Students = list
+            //};
+            var xml = GetListToXml2<List<Student>>(list);
+            File.WriteAllText(fileName, xml);
+            Console.WriteLine(xml);
             Console.ReadKey();
         }
     }
--- XMLTest/Student.cs
+++ XMLTest/Student.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace XMLTest {
-    class Student {
+    [Serializable]
+    [XmlType("Student")]
+    public class Student {
+        [XmlElement("Name")]
         public string Name { get; set; }
+        [XmlElement("Location")]
         public string Location { get; set; }
 
         public static void PrintList(List<Student> students) {
